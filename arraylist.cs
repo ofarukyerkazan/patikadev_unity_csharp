be31181eed8466ce7e0540d28aada0a85868c88c
ö.farukyerkazan <61603088+farukyerkazan@users.noreply.github.com>
2023-07-19 17:49:45 +0300

    Add files via upload

---
+++ arraylist.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace arraylist;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // System.Collections namespace
+        ArrayList liste = new ArrayList();
+        liste.Add("Ömer");
+        liste.Add(2);
+        liste.Add(true);
+        liste.Add('M');
+
+        //içerisinde verilere erişim
+        System.Console.WriteLine(liste[1]);
+        foreach(var item in liste)
+            System.Console.WriteLine(item);
+        
+        //AddRange
+        System.Console.WriteLine("***** AddRange *****");
+        string[] renkler = {"kırmızı","sarı","yeşil"};
+        List<int> sayilar = new List<int>(){1,8,3,7,9,92,5};
+        liste.AddRange(renkler);
+        liste.AddRange(sayilar);
+
+        foreach(var item in liste)
+            System.Console.WriteLine(item);
+
+        //Sort
+        System.Console.WriteLine("***** Sort *******");
+        liste.Sort();
+    }
+}
