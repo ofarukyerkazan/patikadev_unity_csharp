d51f2ad40e5b84098ea106637c4943b8ae7484ee
ö.farukyerkazan <61603088+farukyerkazan@users.noreply.github.com>
2023-07-24 18:13:27 +0300

    Add files via upload

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+namespace Koleksiyonlar_Soru_3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Bir kelime giriniz");
+        string cumle = Console.ReadLine().ToLower();
+        ArrayList list = new ArrayList();
+        foreach (char harfler in cumle)
+        {
+            if (sesli(harfler))
+            {
+                if (!list.Contains(harfler))
+                {
+                    list.Add(harfler);
+                }
+            }
+        }
+        list.Sort();
+        System.Console.WriteLine("Cümledeki sesli harfler");
+        foreach (char s in list )
+        {
+            System.Console.Write(s+" ");
+        }
+    }
+
+    static bool sesli(char harfler)
+    {
+        return "aeiöoüu".Contains(harfler);
+    }
+}
