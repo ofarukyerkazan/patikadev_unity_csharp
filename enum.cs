5b23026245d93c9dbc0bf649fad39f60314a05ea
ö.farukyerkazan <61603088+farukyerkazan@users.noreply.github.com>
2023-07-26 16:46:40 +0300

    Add files via upload

---
+++ enum.cs
@@ -0,0 +1,34 @@
+﻿namespace enums;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Gunler.Pazar);
+        Console.WriteLine((int)Gunler.Perşembe);
+
+        int sıcaklık =32;
+        if(sıcaklık<=(int)havaDurumu.Normal)
+            System.Console.WriteLine("Dışarı çıkmak için havanın biraz daha ısınmasını bekleyelim");
+        else if(sıcaklık>=(int)havaDurumu.Sıcak)
+            System.Console.WriteLine("Dışarı çıkmak için çok sıcak bir gün");
+        else if(sıcaklık>=(int)havaDurumu.Normal && sıcaklık<(int)havaDurumu.Coksıcak)
+            System.Console.WriteLine("Hadi dışarıya çıkalım!");
+    }
+}
+enum Gunler
+{
+    Pazartesi,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+}
+enum havaDurumu
+{
+    Soguk =5,
+    Normal = 20,
+    Sıcak = 25,
+    Coksıcak = 30
+}
